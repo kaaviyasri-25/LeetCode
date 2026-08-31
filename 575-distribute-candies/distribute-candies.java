62b5effa12eaa11a5e81305a5ebc84273e2c124e
kaaviyasri-25 <kaaviyasri2007@gmail.com>
2026-08-31 22:39:53 +0530

    Time: 31 ms (56.68%) | Memory: 48.6 MB (49.89%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+        
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
